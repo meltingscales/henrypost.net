5c0ca6cddfe09d3d6b4eb990cba9bd2d1b049f1c
Henry Post <HenryFBP@gmail.com>
2022-08-01 11:44:55 -0400

    sample data model

--- src/pages/hobbies/Books.tsx
+++ src/pages/hobbies/Books.tsx
@@ -1,13 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const bookData = [
+    {
+        title: "Coding for Aardvarks",
+        isbn9: '12345667',
+        review: "9/10",
+        thoughts: "A solid beginner book for Aardvark programming. Don't buy new, it's useful to have as a reference manual though.",
+        tags: ['programming', 'aarvark', 'beginner-programming']
+    }
+]
+
 const Books = () => {
 
     return <>
         <h1>books</h1>
         <div>some book list
-        1
-        2
-        3
-        4
-        5</div>
+            1
+            2
+            3
+            4
+            5
+        </div>
     </>;
 }
 
