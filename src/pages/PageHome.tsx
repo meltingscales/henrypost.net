94dbe03be4aa395b74c834590cf3482d27e08c38
henryfbp <HenryFBP@gmail.com>
2022-09-08 19:24:07 -0400

    improve homepage

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,23 +1,83 @@
 import React from 'react';
-import {Alert, Container} from "react-bootstrap";
+import {Alert, Card, Col, Container, ListGroup, Row} from "react-bootstrap";
 import {HENRY_ORIGINAL_WEBSITE_URL, OWNER_NAME} from "../Config";
+
+
+const ListGroupItemLink = (props: any) => {
+    return <a href={props.link}>
+        <ListGroup.Item action>
+            {props.text}
+        </ListGroup.Item>
+    </a>
+}
 
 const PageHome = () => {
     // noinspection HttpUrlsUsage
     return <Container>
 
         <Container>
-            <h1>{OWNER_NAME}</h1>
-            <h2>Senior Security Engineer, U.S. Bank</h2>
-            <img style={{width: '20vw'}}
-                 src={require('../img/headshot.png')}
-                 alt={'Henry Post smiling, wearing a bright orange shirt, standing in front of the sea near Hawaii.'}/>
-            <ul>
-                <li><a href={'mailto:resplendent.falconeer@gmail.com'}>Email</a></li>
-                <li><a href={'https://github.com/HenryFBP'}>GitHub</a></li>
-                <li><a href={'https://github.com/HenryFBP/dotfiles/'}>Dotfiles (Got a new laptop?)</a></li>
-            </ul>
+
+            <Row>
+                <Col>
+                    <Card
+                        // style={{width: '30vw'}}
+                    >
+                        <Card.Img
+                            src={require('../img/headshot.png')}
+                            alt={'Henry Post smiling, wearing a bright orange shirt, standing in front of the sea near Hawaii.'}
+                        />
+                        <Card.Body>
+                            <Card.Title>
+                                <h1>{OWNER_NAME}</h1>
+                                <h2>Senior Security Engineer</h2>
+                                <h3>U.S. Bank</h3>
+                            </Card.Title>
+                            <Card.Text>
+                                <ListGroup>
+                                    <ListGroupItemLink
+                                        link={'mailto:resplendent.falconeer@gmail.com'}
+                                        text={'Email'}/>
+                                    <ListGroupItemLink
+                                        link={'https://github.com/HenryFBP'}
+                                        text={'Github'}/>
+                                    <ListGroupItemLink
+                                        link={'https://github.com/HenryFBP/dotfiles/'}
+                                        text={'Dotfiles (Got a new laptop?)'}/>
+                                </ListGroup>
+                            </Card.Text>
+                        </Card.Body>
+
+                    </Card>
+                </Col>
+                <Col>
+                    <Container>
+                        <h1>Self Introduction</h1>
+                        <p>I’m a Senior Security Engineer at U.S. Bank, Illinois Institute of Technology alumni, and
+                            used to work as
+                            a private tutor for Python, Java, and web development.
+                        </p>
+
+                        <p> Working full time at U.S. Bank currently and doing a master’s program at NYU, so I will not
+                            be updating
+                            this site very much.
+                        </p>
+                        <p>
+                            In my free time I like to bake, go cycling, play video games, and scuba dive (if I have
+                            enough time).
+                        </p>
+                        <p>
+                            <a href={'https://github.com/HenryFBP/'}>Check out my GitHub</a> for code that I make, and
+                            message me if
+                            you’d like to get tutored on something or
+                            have any questions.
+                        </p>
+                    </Container>
+                </Col>
+            </Row>
+
         </Container>
+
+        <hr/>
 
         <Container>
             <Alert variant={"info"}>
@@ -33,8 +93,7 @@ const PageHome = () => {
                 <p>Hello, welcome to {OWNER_NAME}'s website.</p>
                 <p>I am learning ReactJS, which this website is written in.</p>
                 <p>This is intended to replace <a href={'https://henryfbp.github.io/'}>my old website</a>, which has
-                    grown
-                    very large and is somewhat hard to customize
+                    grown very large and is somewhat hard to customize
                     as it was built with Hugo, a static Markdown to HTML templating tool.</p>
             </Alert>
             <Alert variant={'danger'}>
@@ -45,24 +104,6 @@ const PageHome = () => {
             </Alert>
         </Container>
 
-        <Container>
-            <h1>Self Introduction</h1>
-            <p>I’m a Senior Security Engineer at U.S. Bank, Illinois Institute of Technology alumni, and used to work as
-                a private tutor for Python, Java, and web development.
-            </p>
-
-            <p> Working full time at U.S. Bank currently and doing a master’s program at NYU, so I will not be updating
-                this site very much.
-            </p>
-            <p>
-                In my free time I like to bake, go cycling, play video games, and scuba dive (if I have enough time).
-            </p>
-            <p>
-                <a href={'https://github.com/HenryFBP/'}>Check out my GitHub</a> for code that I make, and message me if
-                you’d like to get tutored on something or
-                have any questions.
-            </p>
-        </Container>
 
         <Container>
             <script src="https://unpkg.com/github-card@1.2.1/dist/widget.js"></script>
