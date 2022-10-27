0fb066f6dd6a908d44d272affaf8dfef707e0653
Henry Post <HenryFBP@gmail.com>
2022-10-27 14:28:33 -0400

    add todo for links

--- src/pages/fun/PageCoolLinks.tsx
+++ src/pages/fun/PageCoolLinks.tsx
@@ -6,6 +6,8 @@ const PageCoolLinks = () => {
         <h1>cool links</h1>
         <div>some link list :)
         </div>
+        <p>https://github.com/lynn/lynn.github.io</p>
+        <p>TODO copy stuff from https://github.com/HenryFBP/henryfbp.github.io/blob/master/content/cool-links/_index.md</p>
     </>;
 }
 
