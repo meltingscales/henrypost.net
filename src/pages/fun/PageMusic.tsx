eb3ecb3d922e29e104796d09a89a613264b3ae4c
Henry Post <HenryFBP@gmail.com>
2022-11-01 12:46:42 -0400

    make music list not empty

--- src/pages/fun/PageMusic.tsx
+++ src/pages/fun/PageMusic.tsx
@@ -1,16 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
+import ReactMarkdown from "react-markdown";
+
+import MUSIC_LIST from '../../data/file/musicList.jsx'
+
 const PageMusic = () => {
 
     return <>
         <h1>music</h1>
-        <div>some music list
-            1
-            2
-            3
-            4
-            5
-        </div>
+        <div>TODO: Make this like book reviews page...</div>
+        <ReactMarkdown>
+            {MUSIC_LIST}
+        </ReactMarkdown>
     </>;
 }
 
