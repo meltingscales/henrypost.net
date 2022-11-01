8f5d1fb3371c8b317468175ab4f54445fa0ca139
henryfbp <HenryFBP@gmail.com>
2022-10-31 22:58:03 -0400

    add a few links

--- src/pages/fun/PageCoolLinks.tsx
+++ src/pages/fun/PageCoolLinks.tsx
@@ -7,6 +7,8 @@ const PageCoolLinks = () => {
         <div>some link list :)
         </div>
         <p>https://github.com/lynn/lynn.github.io</p>
+        <p>https://metaphorician.substack.com/p/in-the-belly-of-a-beast</p>
+        <p>Everything Everywhere all at Once</p>
         <p>TODO copy stuff from https://github.com/HenryFBP/henryfbp.github.io/blob/master/content/cool-links/_index.md</p>
     </>;
 }
